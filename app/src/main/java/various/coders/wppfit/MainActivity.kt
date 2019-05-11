8479dbd8ea12656df5aaeb0435470c5bb89247ca
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2019-05-11 10:10:18 +0200

    added all the basic fragments

--- app/src/main/java/various/coders/wppfit/MainActivity.kt
+++ app/src/main/java/various/coders/wppfit/MainActivity.kt
@@ -1,7 +1,6 @@
 package various.coders.wppfit
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
@@ -10,6 +9,7 @@ import android.view.Menu
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import various.coders.wppfit.fragments.HomeScreenFragment
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
---
+++ app/src/main/java/various/coders/wppfit/fragments/AddExerciseFragment.kt
@@ -0,0 +1,20 @@
+package various.coders.wppfit.fragments
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import various.coders.wppfit.R
+
+class AddExerciseFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_add_exercise, container, false)
+    }
+
+}
