f3f138d86c4ea8ebf07643cb7cba80de95ce5e19
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2019-05-12 13:55:21 +0200

    added an index on user id

--- app/src/main/java/various/coders/wppfit/model/database/entities/User.kt
+++ app/src/main/java/various/coders/wppfit/model/database/entities/User.kt
@@ -4,9 +4,13 @@ import android.arch.persistence.room.*
 import various.coders.wppfit.model.database.converters.TimeConverter
 import java.util.*
 
-@Entity
+@Entity(
+    indices = [Index(value = ["uid"], unique = true)]
+)
 data class User(
-    @PrimaryKey(autoGenerate = true) val uid: Int,
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "uid")
+    val uid: Int,
     @ColumnInfo(name = "first_name") val firstName: String,
     @ColumnInfo(name = "last_name") val lastName: String,
     @ColumnInfo(name = "gender") val gender: Boolean, //true for M, false for F
