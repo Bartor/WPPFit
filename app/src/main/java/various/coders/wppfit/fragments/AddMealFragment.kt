bfbd8ad7b63350c7d5eb6aa0969cfdf440655bee
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2019-05-15 13:40:29 +0200

    readied for inserting meals

--- app/src/main/java/various/coders/wppfit/fragments/AddMealFragment.kt
+++ app/src/main/java/various/coders/wppfit/fragments/AddMealFragment.kt
@@ -1,5 +1,6 @@
 package various.coders.wppfit.fragments
 
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -7,12 +8,21 @@ import android.view.View
 import android.view.ViewGroup
 
 import various.coders.wppfit.R
+import various.coders.wppfit.model.AppViewModel
 
 class AddMealFragment : Fragment() {
+    private lateinit var viewModel: AppViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_add_meal, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        viewModel = ViewModelProviders.of(activity!!).get(AppViewModel::class.java)
     }
 }
